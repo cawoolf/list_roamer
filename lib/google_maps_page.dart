822c3ec28e24c7f28b03463fc061419d4127637c
cawoolf <rayadevinfo@gmail.com>
2023-11-21 22:43:48 -0700

    json markers - 4 - Json file loading

--- lib/google_maps_page.dart
+++ lib/google_maps_page.dart
@@ -2,9 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:the_spotz/helpers/json_reader.dart';
+import 'package:flutter/services.dart';
 
-import 'models/peak_marker.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -21,101 +20,16 @@ class _MapScreenState extends State<MapScreen> {
     loadMarkers();
   }
 
-  void loadMarkers() async {
-    // Load your JSON data (replace this with your actual file reading logic)
-    String jsonString = '''
-     {
-  "peaks": [
-    {
-      "name": "Mount Elbert",
-      "elevation": 14440,
-      "coordinates": {
-        "latitude": 39.1178,
-        "longitude": -106.4454
-      }
-    },
-    {
-      "name": "Mount Massive",
-      "elevation": 14421,
-      "coordinates": {
-        "latitude": 39.1875,
-        "longitude": -106.4753
-      }
-    },
-    {
-      "name": "Mount Harvard",
-      "elevation": 14420,
-      "coordinates": {
-        "latitude": 38.9244,
-        "longitude": -106.3208
-      }
-    },
-    {
-      "name": "Blanca Peak",
-      "elevation": 14351,
-      "coordinates": {
-        "latitude": 37.5775,
-        "longitude": -105.4856
-      }
-    },
-    {
-      "name": "La Plata Peak",
-      "elevation": 14336,
-      "coordinates": {
-        "latitude": 39.0294,
-        "longitude": -106.4727
-      }
-    },
-    {
-      "name": "Uncompahgre Peak",
-      "elevation": 14309,
-      "coordinates": {
-        "latitude": 38.0717,
-        "longitude": -107.4629
-      }
-    },
-    {
-      "name": "Crestone Peak",
-      "elevation": 14294,
-      "coordinates": {
-        "latitude": 37.9665,
-        "longitude": -105.5858
-      }
-    },
-    {
-      "name": "Mount Lincoln",
-      "elevation": 14286,
-      "coordinates": {
-        "latitude": 39.3514,
-        "longitude": -106.1111
-      }
-    },
-    {
-      "name": "Grays Peak",
-      "elevation": 14270,
-      "coordinates": {
-        "latitude": 39.6339,
-        "longitude": -105.8174
-      }
-    },
-    {
-      "name": "Mount Antero",
-      "elevation": 14269,
-      "coordinates": {
-        "latitude": 38.6748,
-        "longitude": -106.2467
-      }
-    }
-  ]
-}
-    ''';
+  Future<String> _loadJsonData() async {
+    return await rootBundle.loadString('assets/json_data/peak_location.json');
+  }
 
-    // Parse JSON
+Future<void> loadMarkers() async {
+  try {
+    String jsonString = await _loadJsonData();
     Map<String, dynamic> data = json.decode(jsonString);
     List<dynamic> peaks = data['peaks'];
-    print(peaks);
 
-    // Create markers
     List<Marker> newMarkers = peaks.map((peak) {
       return Marker(
         markerId: MarkerId(peak['name']),
@@ -124,23 +38,30 @@ class _MapScreenState extends State<MapScreen> {
       );
     }).toList();
 
-    // Update the markers state
     setState(() {
       markers = newMarkers;
     });
+  } catch (e) {
+    print('Error loading JSON data: $e');
   }
+}
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      onMapCreated: (GoogleMapController controller) {
-        mapController = controller;
-      },
-      initialCameraPosition: CameraPosition(
-        target: LatLng(39.1178, -106.4454), // Initial camera position
-        zoom: 6.0,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('The Spotz - Colorado Peaks'),
       ),
-      markers: Set<Marker>.of(markers),
+      body: GoogleMap(
+        onMapCreated: (GoogleMapController controller) {
+          mapController = controller;
+        },
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(39.1178, -106.4454), // Initial camera position
+          zoom: 6.0,
+        ),
+        markers: Set<Marker>.of(markers),
+      ),
     );
   }
 }
