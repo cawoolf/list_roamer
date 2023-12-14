1c3b31416065e9d74ca2328a109ffc7e7d51b197
cawoolf <rayadevinfo@gmail.com>
2023-12-13 17:00:09 -0700

    Firebase test is working

--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -21,9 +22,38 @@ class MapPageState extends State<MapPage> {
   @override
   void initState() {
     super.initState();
-    loadMarkers();
+    // loadMarkers();
+    fireBaseReadTest();
   }
 
+  Scaffold fireBaseReadTest() {
+
+    String locationMarkerCollectionPath = '/users/testUser/lists/testList_1/location_markers';
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text("Firebase Read Test"),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection(locationMarkerCollectionPath).snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView(
+            children: snapshot.data!.docs.map((document) {
+              return Center(child: Text(document['name']));
+            }).toList(),
+          );
+        },
+      ),
+    );
+
+  }
 
   Future<void> loadMarkers() async {
     try {
@@ -43,22 +73,30 @@ class MapPageState extends State<MapPage> {
     }
   }
 
+
+  // Firebase Read Test Build
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('ListRoamer - Colorado Peaks'),
-      ),
-      body: GoogleMap(
-        onMapCreated: (GoogleMapController controller) {
-          mapController = controller;
-        },
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(39.1178, -106.4454), // Initial camera position
-          zoom: 6.0,
-        ),
-        markers: Set<Marker>.of(markers),
-      ),
-    );
+    return fireBaseReadTest();
   }
+
+  // Original build method
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: const Text('ListRoamer - Colorado Peaks'),
+  //     ),
+  //     body: GoogleMap(
+  //       onMapCreated: (GoogleMapController controller) {
+  //         mapController = controller;
+  //       },
+  //       initialCameraPosition: const CameraPosition(
+  //         target: LatLng(39.1178, -106.4454), // Initial camera position
+  //         zoom: 6.0,
+  //       ),
+  //       markers: Set<Marker>.of(markers),
+  //     ),
+  //   );
+  // }
 }
