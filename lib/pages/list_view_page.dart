780ecb956e57938565f6c10b561238a0993177a9
cawoolf <rayadevinfo@gmail.com>
2023-12-13 17:39:48 -0700

    Added read to lists

--- lib/pages/list_view_page.dart
+++ lib/pages/list_view_page.dart
@@ -1,42 +1,88 @@
 import 'dart:js';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
 import '../services/database.dart';
 
-class ListViewPage extends StatelessWidget {
+class ListViewPage extends StatefulWidget {
   const ListViewPage({super.key});
 
+  @override
+  State<StatefulWidget> createState() => ListViewPageState();
+}
+
+class ListViewPageState extends State<ListViewPage> {
+  @override
+  void initState() {
+    super.initState();
+    fireBaseReadTest();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final database = Provider.of<Database>(context, listen: false);
-    var testMarker = database.getTestMarker();
-    print(testMarker.first);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('ListView Page'),
-      ),
-      body: buildListView(),
-    );
+    return fireBaseReadTest();
   }
 
-  ListView buildListView() {
+  ListView buildListView(
+      String name, String snippet, String latitude, String longitude) {
     return ListView(
-      children: const <Widget>[
+      children: <Widget>[
         ListTile(
-          title: Text('Item 1'),
+          title: Text(name),
         ),
         ListTile(
-          title: Text('Item 2'),
+          title: Text(snippet),
         ),
         ListTile(
-          title: Text('Item 3'),
+          title: Text(latitude),
+        ),
+        ListTile(
+          title: Text(longitude),
         ),
         // Add more ListTiles as needed
       ],
+    );
+  }
+
+  Scaffold fireBaseReadTest() {
+    String locationCollectionPath =
+        '/users/testUser/lists/testList_1/location_markers';
+
+    String listCollectionPath =
+        '/users/testUser/lists';
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Firebase Read Test"),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection(listCollectionPath)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView(
+            children: snapshot.data!.docs.map((document) {
+              return Center(child: Text(document['title']));
+            }).toList(),
+          );
+          // return ListView.builder(
+          //     itemCount: snapshot.data!.docs.length,
+          //     itemBuilder: (context, index) {
+          //       var document = snapshot.data!.docs[index];
+          //       return buildListView(document['name'], document['snippet'],
+          //           document['latitude'], document['longitude']);
+          //     });
+        },
+      ),
     );
   }
 }
--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -22,37 +22,8 @@ class MapPageState extends State<MapPage> {
   @override
   void initState() {
     super.initState();
-    // loadMarkers();
-    fireBaseReadTest();
-  }
-
-  Scaffold fireBaseReadTest() {
-
-    String locationMarkerCollectionPath = '/users/testUser/lists/testList_1/location_markers';
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: const Text("Firebase Read Test"),
-      ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection(locationMarkerCollectionPath).snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          return ListView(
-            children: snapshot.data!.docs.map((document) {
-              return Center(child: Text(document['name']));
-            }).toList(),
-          );
-        },
-      ),
-    );
-
+    loadMarkers();
+    // fireBaseReadTest();
   }
 
   Future<void> loadMarkers() async {
@@ -74,29 +45,29 @@ class MapPageState extends State<MapPage> {
   }
 
 
-  // Firebase Read Test Build
-  @override
-  Widget build(BuildContext context) {
-    return fireBaseReadTest();
-  }
-
-  // Original build method
+  // // Firebase Read Test Build
   // @override
   // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: const Text('ListRoamer - Colorado Peaks'),
-  //     ),
-  //     body: GoogleMap(
-  //       onMapCreated: (GoogleMapController controller) {
-  //         mapController = controller;
-  //       },
-  //       initialCameraPosition: const CameraPosition(
-  //         target: LatLng(39.1178, -106.4454), // Initial camera position
-  //         zoom: 6.0,
-  //       ),
-  //       markers: Set<Marker>.of(markers),
-  //     ),
-  //   );
+  //   return fireBaseReadTest();
   // }
+
+  // Original build method
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListRoamer - Colorado Peaks'),
+      ),
+      body: GoogleMap(
+        onMapCreated: (GoogleMapController controller) {
+          mapController = controller;
+        },
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(39.1178, -106.4454), // Initial camera position
+          zoom: 6.0,
+        ),
+        markers: Set<Marker>.of(markers),
+      ),
+    );
+  }
 }
