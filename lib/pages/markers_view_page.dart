4aa82ec5cdecbf304cc7f1525217a4059e46d2f5
cawoolf <rayadevinfo@gmail.com>
2023-12-14 18:34:11 -0700

    displaying list view details on click

--- lib/pages/markers_view_page.dart
+++ lib/pages/markers_view_page.dart
@@ -1,26 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-
 class MarkersViewPage extends StatelessWidget {
   final QueryDocumentSnapshot<Object?> document;
 
   const MarkersViewPage({Key? key, required this.document}) : super(key: key);
-
-  ListView buildListView(String name, String snippet, String latitude, String longitude) {
-    return ListView(
-      children: [
-        ListTile(
-          title: Text('Field 1: ${document['field1']}'),
-        ),
-        ListTile(
-          title: Text('Field 2: ${document['field2']}'),
-        ),
-        // Add more ListTiles for other fields as needed
-      ],
-    );
-  }
-
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +12,6 @@ class MarkersViewPage extends StatelessWidget {
     String documentId = 'testList_1';
     String locationCollectionPath =
         '/users/testUser/lists/$documentId/location_markers';
-
 
     return Scaffold(
       appBar: AppBar(
@@ -47,11 +30,22 @@ class MarkersViewPage extends StatelessWidget {
 
           return ListView(
             children: snapshot.data!.docs.map((document) {
-              return Center(child: Text(document['name']));
+              return Column(
+                children: buildMarkerListView(document),
+              );
             }).toList(),
           );
         },
       ),
     );
+  }
+
+  List<Widget> buildMarkerListView(QueryDocumentSnapshot<Object?> document) {
+    return [
+      ListTile(title: Text(document['name'])),
+      ListTile(title: Text(document['snippet'])),
+      ListTile(title: Text(document['latitude'])),
+      ListTile(title: Text(document['longitude'])),
+    ];
   }
 }
