81f428a304035f6cd0aa859385faee2584bda96c
cawoolf <rayadevinfo@gmail.com>
2023-12-17 19:29:58 -0700

    api path fix

--- lib/services/api_path.dart
+++ lib/services/api_path.dart
@@ -4,6 +4,6 @@ class APIPath {
   // /users/testUser/lists/testList_1/location_markers/test_location_1
   static String locationMarkers({required String listId}) => 'users/testUser/list/$listId/location_markers/'; // Returns all location Markers for the list
   static String location({required String listId, required String locationId}) => 'users/testUser/list/$listId/location_markers/$locationId'; // Returns a target location Markers from the list
-  static String userLists({required String userId}) => 'users/$userId/list/';
+  static String userLists({required String userId}) => 'users/$userId/lists/';
 
 }
