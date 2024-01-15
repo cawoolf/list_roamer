c5d89585e5dd13f3b9de853a37180f883e39de93
cawoolf <rayadevinfo@gmail.com>
2024-01-14 19:28:11 -0700

    basic script working

--- assets/scripts/seed_firebase.dart
+++ assets/scripts/seed_firebase.dart
@@ -1,49 +1,37 @@
-// Script for seeding the Firebase DB with JSON Data
-import 'package:list_roamer/model/user_list.dart';
-import 'package:list_roamer/model/user_marker.dart';
-import 'package:list_roamer/services/database.dart';
-import 'package:list_roamer/services/helpers/json_helper.dart';
-import 'package:list_roamer/services/helpers/json_marker_helper.dart';
-
+import 'package:firedart/firedart.dart';
 
 Future<void> main() async {
-  print('seed firebase script running');
-  Database database = FirestoreDatabase(uid: 'testUser');
-  writeTestList(database);
+  Firestore.initialize("listroamer");
+
+  readTest();
+  writeTest();
+
 }
 
-void writeTestList(Database database) {
-  print('Add more lists tapped!');
-  UserList testWrite = UserList(title: 'Test Script Write',category: 'testing',id: documentIdFromCurrentDate());
-  database.setUserList(testWrite);
+void readTest() async {
+  var map = await Firestore.instance.collection("users").get();
+  print(map.toString());
+  var path = '/users/testUser/lists/testList_3/location_markers/Blanca Peak';
+  var peak = await Firestore.instance.document(path).get();
+  print(peak.toString());
+  Firestore.instance.close();
 }
 
-
-// Methods for Markers
-void addMarkersToListTest(UserList userList, Database database) async {
-  print('Add button tapped in MarkersViewPage');
-  // Read the JSON Data and turn it into Markers
-  String peaksAssetRoute = 'assets/json_data/peak_location.json'; // Peak data\
-  String peaksDataName = 'peaks';
-  String sushiAssetRoute = 'assets/json_data/sushi_location.json';
-  String sushiDataName = 'sushi';
-
-  List<UserMarker> jsonMarkers = await createMarkersFromJson(sushiAssetRoute, sushiDataName);
-  // List<UserMarker> jsonMarkers = await createMarkersFromJson(peaksAssetRoute, peaksDataName);
-  // print(jsonMarkers);
-  // Write those Markers to the correct list in Firebase
-  writePeaksMarkersToFirebase(jsonMarkers, userList, database);
-}
-
-Future<List<UserMarker>> createMarkersFromJson(String assetRoute, String dataName) async {
-  Future<List<dynamic>> jsonData = JSONHelper.loadJsonData(assetRoute: assetRoute, dataName: dataName);
-  List<dynamic> data = await jsonData;
-  List<UserMarker> jsonMarkers = JsonMarkerHelper.getJsonMarkers(jsonData: data);
-  return jsonMarkers;
-}
-
-void writePeaksMarkersToFirebase(List<UserMarker> jsonMarkers, UserList userList, Database database) {
-  for (var marker in jsonMarkers) {
-    database.setUserMarker(marker, userList, marker.markerId);
+void writeTest() async {
+  var path = '/users/testUser/lists/scriptTestList/location_markers/Flutter Peak 2';
+  Map<String, dynamic> data = {
+    "name": "Flutter Peak 2",
+    "snippet": "Elevation 15000",
+    "latitude": 40.7128,
+    "longitude": -74.0060,
+  };
+  final reference = Firestore.instance.document(path);
+  print('$path: $data');
+  try {
+    await reference.set(data);
+  } catch (e) {
+    print("Firestore Set Error: $e");
   }
+  Firestore.instance.close();
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:list_roamer/pages/navigation_page.dart';
 import 'package:list_roamer/pages/welcome_page.dart';
 import 'package:list_roamer/services/firebase_options.dart';
-import 'dart:ui';
 
 Future<void> main() async {
 
