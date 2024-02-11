f2b09d3d6d0ee2e5123e4641f5d36ec31bb84509
cawoolf <rayadevinfo@gmail.com>
2024-02-10 20:47:07 -0700

    add places modal progress

--- lib/ui_widgets/add_place_modal.dart
+++ lib/ui_widgets/add_place_modal.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AddPlaceModal extends StatefulWidget {
   const AddPlaceModal({super.key});
+
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  State<StatefulWidget> createState() => _AddPlaceModalState();
+}
+
+  class _AddPlaceModalState extends State<AddPlaceModal> {
+  @override
+  Widget build(BuildContext context) {
+    return _buildAddPlaceModal();
+  }
+
+  Widget _buildAddPlaceModal() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        _buildCard(Icons.access_alarm, 'Card 1'),
+        _buildCard(Icons.access_time, 'Card 2'),
+        _buildCard(Icons.accessibility, 'Card 3'),
+      ],
+    );
+  }
+
+  Widget _buildCard(IconData iconData, String text) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              iconData,
+              size: 48,
+            ),
+            SizedBox(height: 8),
+            Text(
+              text,
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
 
 /*
 Need to show a modal with three small square cards.
