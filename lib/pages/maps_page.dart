0cb264c6d7d1f8fc9f0fbee94f73537ab4fcc813
cawoolf <rayadevinfo@gmail.com>
2024-02-12 21:24:38 -0700

    fixed bugz and added modal

--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -1,24 +1,21 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import '../model/user_list.dart';
 import '../model/user_marker.dart';
 import '../services/database.dart';
+import '../ui_widgets/add_places_modal.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({Key? key, required this.userList}) : super(key: key);
   final UserList? userList;
 
-
   @override
   Widget build(BuildContext context) {
-
     final database = Provider.of<Database>(context, listen: false);
 
     String? listId = userList?.id;
     String? listTitle = userList?.title;
-    // print('maps_page.dart -> $listId');
 
     return Scaffold(
       appBar: AppBar(
@@ -34,19 +31,53 @@ class MapPage extends StatelessWidget {
             return Text('Error: ${snapshot.error}');
           } else {
             List<UserMarker> markers = snapshot.data ?? [];
-            // print('maps_page.dart -> $markers');
-            return GoogleMap(
-              onMapCreated: (GoogleMapController controller) {
-              },
-              initialCameraPosition: const CameraPosition(
-                target: LatLng(39.1178, -106.4454),
-                zoom: 6.0,
-              ),
-              markers: Set<Marker>.of(markers),
+            return Stack(
+              children: [
+                GoogleMap(
+                  onMapCreated: (GoogleMapController controller) {},
+                  initialCameraPosition: const CameraPosition(
+                    target: LatLng(39.1178, -106.4454),
+                    zoom: 6.0,
+                  ),
+                  markers: Set<Marker>.of(markers),
+                ),
+                Positioned(
+                  bottom: 54.0,
+                  right: 64.0,
+                  child: FloatingActionButton(
+                    onPressed: () {
+                      // Add your onPressed functionality here
+                      // triggerAddPlaceModal(context);
+                      _showAddPlacesDialog(context);
+                    },
+                    child: Icon(Icons.add),
+                  ),
+                ),
+              ],
             );
           }
         },
       ),
+    );
+  }
+
+  void triggerAddPlaceModal(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return const AddPlaceModal();
+      },
+    );
+  }
+
+  void _showAddPlacesDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return const Dialog(
+          child: AddPlaceModal(),
+        );
+      },
     );
   }
 }
---
+++ lib/ui_widgets/add_places_modal.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceModal extends StatefulWidget {
+  const AddPlaceModal({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _AddPlaceModalState();
+}
+
+class _AddPlaceModalState extends State<AddPlaceModal> {
+  @override
+  Widget build(BuildContext context) {
+    return _buildAddPlaceModal();
+  }
+
+  Widget _buildAddPlaceModal() {
+    return Container(
+      width: 600,
+      height: 350,
+      decoration: BoxDecoration(
+        color: Colors.grey[200], // Set background color
+        borderRadius: BorderRadius.circular(8.0), // Set border radius
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildCard(Icons.access_alarm, 'Card 1'),
+                const SizedBox(width: 16), // Adjust spacing between cards
+                _buildCard(Icons.access_time, 'Card 2'),
+              ],
+            ),
+            const SizedBox(height: 32), // Adjust vertical space between rows
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildCard(Icons.accessibility, 'Card 3'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard(IconData iconData, String text) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              iconData,
+              size: 48,
+            ),
+            SizedBox(height: 8),
+            Text(
+              text,
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+/*
+Need to show a modal with three small square cards.
+Share a location from Google Maps
+Search for a location from Places API
+Save and add the current GPS Location
+  > Try to find the relevant place in Places API?
+
+Modal can be created from two places?
+On the Maps Page
+  > Adds a place to whichever list is loaded on the Map
+After selecting a List and landing on the MarkersViewPage
+  > Can click add and create the modal and add to the current list
+ */
